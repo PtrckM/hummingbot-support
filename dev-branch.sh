e4a8d75a6684da4f279827c486eaa76a7b6581d8
Patrick <54516858+PtrckM@users.noreply.github.com>
2019-11-27 12:17:23 +0800

    (sh) for testing dev-branch

---
+++ dev-branch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export CONDAPATH="$(pwd)/miniconda3"
+export PYTHON="$(pwd)/miniconda3/envs/hummingbot/bin/python3"
+export hummingbotPath="$(pwd)/hummingbot" && cd $hummingbotPath
+# REMOVE OLD INSTALLATION
+${CONDAPATH}/bin/deactivate
+rm -rf ../miniconda3/envs/hummingbot
+./uninstall
+./clean
+# UPDATE HUMMINGBOT
+# Download latest code
+git pull origin development
+git fetch origin
+git branch -a
+# Test branch
+echo "Enter Branch Name: "
+read branch_name
+git checkout $branch_name
+git pull origin $branch_name
+./install
+${CONDAPATH}/bin/activate hummingbot
+# compile
+./compile
+# start hummingbot
+${PYTHON} bin/hummingbot.py
+
+
