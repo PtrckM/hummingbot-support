d19d3d4b60811fdee1176777487be2c373bbada3
Patrick <54516858+PtrckM@users.noreply.github.com>
2020-01-13 10:30:51 +0800

    (sh) master-update

--- master-update.sh
+++ master-update.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# for downloading latest release thru source. 
+# make sure your not inside `hummingbot/` folder
+# usage: wget https://raw.githubusercontent.com/PtrckM/hummingbot-support/master/master-update.sh
+#        chmod a+x master-update.sh
+#       ./master-update.sh
 
 export CONDAPATH="$(pwd)/miniconda3"
 export PYTHON="$(pwd)/miniconda3/envs/hummingbot/bin/python3"
 export hummingbotPath="$(pwd)/hummingbot" && cd $hummingbotPath
-# REMOVE OLD INSTALLATION
 ${CONDAPATH}/bin/deactivate
-rm -rf ../miniconda3/envs/hummingbot
+rm -rf $(pwd)/miniconda3/envs/hummingbot
 ./uninstall
 ./clean
-# UPDATE HUMMINGBOT
-# Download latest code
 git pull origin master
 git fetch origin
 ./install
