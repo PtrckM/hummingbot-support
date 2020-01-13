e828c427c609d84de2ca85454ac332f99a36dcbd
Patrick <54516858+PtrckM@users.noreply.github.com>
2020-01-13 10:52:40 +0800

    (sh) dev-update

--- dev-update.sh
+++ dev-update.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+# for testing current availalable branch thru dev. 
+# make sure your not inside `hummingbot/` folder
+# usage: wget https://raw.githubusercontent.com/PtrckM/hummingbot-support/master/dev-update.sh
+#        chmod a+x dev-update.sh
+#       ./dev-update.sh
 
+echo -ne "\n[] - setting up configurations...\n"
 export CONDAPATH="$(pwd)/miniconda3"
 export PYTHON="$(pwd)/miniconda3/envs/hummingbot/bin/python3"
 export hummingbotPath="$(pwd)/hummingbot" && cd $hummingbotPath
-# REMOVE OLD INSTALLATION
+echo -ne "\n[] - deactivating conda hummingbot...\n"
 ${CONDAPATH}/bin/deactivate
-rm -rf ../miniconda3/envs/hummingbot
+echo -ne "\n[] - removing old environments...\n"
+rm -rf $(pwd)/miniconda3/envs/hummingbot
+echo -ne "\n[] - uninstalling...\n"
 ./uninstall
+echo -ne "\n[] - housekeeping...\n"
 ./clean
-# UPDATE HUMMINGBOT
-# Download latest code
+echo -ne "\n[] - downloading development...\n"
 git pull origin development
+echo -ne "\n[] - switching...\n"
 git fetch origin
+echo -ne "\n[] - installing...\n"
 ./install
 ${CONDAPATH}/bin/activate hummingbot
-# compile
+echo -ne "\n[] - compiling...\n"
 ./compile
-# start hummingbot
+echo -ne "\n[] - starting...\n"
 ${PYTHON} bin/hummingbot.py
 
 
