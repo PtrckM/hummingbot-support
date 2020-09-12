f6ad5b5925e3916d3424c20fb72658b3335ae36b
Patrick <54516858+PtrckM@users.noreply.github.com>
2020-09-12 20:02:18 +0800

    (sh) script downloader for hb docker and source build

---
+++ down-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Downloader Sample Hummingbot scripts
+# this script will download the files in your cwd
+# move the files to the scripts folder
+
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/master/scripts/dynamic_price_band_script.py -o dynamic_price_band_script.py 
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/master/scripts/hello_world_script.py -o hello_world_script.py
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/master/scripts/ping_pong_script.py -o ping_pong_script.py
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/master/scripts/price_band_script.py -o price_band_script.py
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/master/scripts/spreads_adjusted_on_volatility_script.py -o spreads_adjusted_on_volatility_script.py
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/master/scripts/update_parameters_test_script.py -o update_parameters_test_script.py
