91fd0be65c7a56d333748c2ee9d189491c061e98
Patrick <54516858+PtrckM@users.noreply.github.com>
2022-01-12 07:25:31 +0800

    Create cloner.sh

---
+++ cloner.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# this script will download the repo and install its own conda environment
+#
+
+
+read -p "Enter which repo to clone [coinalpha, foundation] (default = \"coinalpha\") >>> " REPO
+if [ "$REPO" == "" ]
+then
+  REPO="coinalpha"
+fi
+
+read -p "Enter branch name (eg. fix/trade_fee) >>> " BRANCH
+
+foldername=$($BRANCH | tr / _)
+git clone -b $BRANCH https://github.com/$REPO/hummingbot $foldername
+
+read -p "Enter name of envinronment (default = \"hummingbot\") >>> " ENV_NAME
+if [ "$ENV_NAME" == "" ]
+then
+  ENV_NAME="hummingbot"
+else
+        sed -i 's/name: hummingbot/name: $ENV_NAME/g' $foldername/setup/environment.yml
+        sed -i 's/name: hummingbot/name: $ENV_NAME/g' $foldername/setup/environment-linux.yml
+        sed -i 's/name: hummingbot/name: $ENV_NAME/g' $foldername/setup/environment-linux-aarch64.yml
+        sed -i 's/name: hummingbot/name: $ENV_NAME/g' $foldername/setup/environment-win64.yml
+fi
+
+cd $foldername
+./install
+conda activate $ENV_NAME
+./compile
+conda env list
