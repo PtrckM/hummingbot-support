f5791da50e127e358c2904f505cbe0db30e52aa8
Patrick <54516858+PtrckM@users.noreply.github.com>
2022-02-02 14:07:42 +0800

    (sh) convert python timestamp to readable format

--- timestamp.sh
+++ timestamp.sh
@@ -14,11 +14,12 @@ if [[ -z "$@" ]]; then
    echo -ne "\n[*] -- Usage: ./timestamp.sh or ./timestamp.sh 1643428161295\n\n"
    echo -ne "[-] -- Enter timestamp >> "
    read timestamp
-   date -d @$timestamp
+   echo -ne "\n[i] -- Readable time ==> "
+   date -d @$(($timestamp/1000))
    echo ""
    exit 1
 else
-   echo -ne "\n[+] -- Readable time ==> "
+   echo -ne "\n[i] -- Readable time ==> "
    date -d @$(($@/1000))
    echo ""
    exit 1
